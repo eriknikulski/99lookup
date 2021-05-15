df5c314702c1f2852b92b874726026c38df36ed2
Erik Nikulski <erik@nikulski.me>
2021-05-15 13:20:30 +0200

    Improve display function

--- 99lookup.js
+++ 99lookup.js
@@ -39,9 +39,7 @@ function getMapName(list, id) {
 }
 
 function getMatchInfo(matchID, teamName) {
-  let bans = [];
-
-  fetch('https://liga.99damage.de/ajax/leagues_match', {
+  return fetch('https://liga.99damage.de/ajax/leagues_match', {
     method: 'post',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
@@ -49,106 +47,91 @@ function getMatchInfo(matchID, teamName) {
     body: `id=${matchID}&action=init&devmode=1&language=de`
   })
     .then(response => response.json())
-    .then(data => {
-      if (data['status'] !== 'finished') return;
-      const _getMapName = (id) => getMapName(data['draft_maps'], id);
-      if (data['draft_opp1'] === teamName) {
-        bans.push(_getMapName(data['draft_mapvoting_bans'][0]));
-        bans.push(_getMapName(data['draft_mapvoting_bans'][3]));
-      } else if (data['draft_opp2'] === teamName) {
-        bans.push(_getMapName(data['draft_mapvoting_bans'][1]));
-        bans.push(_getMapName(data['draft_mapvoting_bans'][2]));
+    .then(json => {
+      let info = [];
+      if (json['status'] !== 'finished') return;
+      const _getMapName = (id) => getMapName(json['draft_maps'], id);
+      if (json['draft_opp1'] === teamName) {
+        info.push(_getMapName(json['draft_mapvoting_bans'][0]));
+        info.push(_getMapName(json['draft_mapvoting_bans'][3]));
+        info.push(_getMapName(json['draft_mapvoting_picks'][1]));
+      } else if (json['draft_opp2'] === teamName) {
+        info.push(_getMapName(json['draft_mapvoting_bans'][1]));
+        info.push(_getMapName(json['draft_mapvoting_bans'][2]));
+        info.push(_getMapName(json['draft_mapvoting_picks'][0]));
       }
+      return info;
     })
     .catch(error => console.log(error));
-
-  return bans;
 }
 
 function getMaps() {
   return ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'];
 }
 
-function displayInfo(data) {
+function displayInfo(data, labels=['Maps', '1st ban', '2nd ban', 'Pick']) {
   let div = document.createElement('div');
+  div.classList.add('l99l-container');
   let table = document.createElement('table');
-  let tableHeader = document.createElement('tr');
+  div.classList.add('l99l-table');
 
-  let th = document.createElement('th');
-  th.textContent = 'Maps';
-  tableHeader.appendChild(th);
-  for (const map in data['map']) {
-    let th = document.createElement('th');
-    th.textContent = map['name'];
-    tableHeader.appendChild(th);
-  }
-  table.appendChild(tableHeader);
 
   let tr = document.createElement('tr');
-  th = document.createElement('th');
-  th.textContent = '1st ban';
+  let th = document.createElement('th');
+  th.textContent = labels[0];
   tr.appendChild(th);
-  for (const map in data['map']) {
-    let td = document.createElement('th');
-    td.textContent = map['ban_1'];
-    tr.appendChild(td);
+  for (const map in data) {
+    let th = document.createElement('th');
+    th.textContent = map;
+    tr.appendChild(th);
   }
   table.appendChild(tr);
 
-  tr = document.createElement('tr');
-  th = document.createElement('th');
-  th.textContent = '2nd ban';
-  tr.appendChild(th);
-  for (const map in data['map']) {
-    let td = document.createElement('th');
-    td.textContent = map['ban_2'];
-    tr.appendChild(td);
-  }
-  table.appendChild(tr);
+  let i = 1;
+  for (const attr in data[Object.keys(data)[0]]) {
+    let tr = document.createElement('tr');
+    let th = document.createElement('th');
+    th.textContent = labels[i];
+    tr.appendChild(th);
 
-  tr = document.createElement('tr');
-  th = document.createElement('th');
-  th.textContent = 'pick';
-  tr.appendChild(th);
-  for (const map in data['map']) {
-    let td = document.createElement('th');
-    td.textContent = map['pick'];
-    tr.appendChild(td);
+    for (const map in data) {
+      let td = document.createElement('th');
+      td.textContent = (data[map][attr] * 100).toFixed() + '%';
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+    i++;
   }
-  table.appendChild(tr);
 
   div.appendChild(table);
-
   document.querySelector('.league-team-stage').appendChild(div);
+}
+
+function round(value) {
+  return Math.round(value * 100) / 100;
 }
 
 function getTeamInfo() {
   const teamName = getTeamName();
   const matches = getMatches();
   const maps = getMaps();
-  let data = {};
 
-  let bans = [];
-
-  for (const i in matches) {
-    bans.push(getMatchInfo(matches[i], teamName));
-  }
-
-  // bans = bans.filter(ban => ban.length !== 0);
-
-  console.log(bans);
-
-  for (let i = 0; i < maps.length; i++) {
-    data[maps[i]] = {
-      'ban_1': bans.map(ban => ban[0]).filter(ban => ban === maps[i]).length,
-      'ban_2': bans.map(ban => ban[1]).filter(ban => ban === maps[i]).length,
-    };
-
-    // data[maps[i]]['ban_1'] = bans.map(ban => ban[0]).filter(ban => ban === maps[i]).length;
-    // data[maps[i]]['ban_2'] = bans.map(ban => ban[1]).filter(ban => ban === maps[i]).length;
-  }
-  console.log(data);
-  console.log(bans);
+  Promise.all(matches.map(match => getMatchInfo(match, teamName)))
+    .then(matches => {
+      let data = {};
+      matches = matches.filter(match => match !== undefined);
+      let count = matches.length;
+      for (let i = 0; i < maps.length; i++) {
+        data[maps[i]] = {
+          'ban_1': round(matches.map(match => match[0]).filter(match => match === maps[i]).length / count),
+          'ban_2': round(matches.map(match => match[1]).filter(match => match === maps[i]).length / count),
+          'pick': round(matches.map(match => match[2]).filter(match => match === maps[i]).length / count),
+        };
+      }
+      console.log(data);
+      return data;
+    })
+    .then(data => displayInfo(data));
 }
 
 getTeamInfo();
